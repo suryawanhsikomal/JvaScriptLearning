e4d700751bd98cef372f001a252283bba8129a39
suryawanhsikomal <komal.suryawanshi21@gmail.com>
2022-12-09 17:39:12 +0530

    using some more concept
    how to know data type,data type values  etc

--- 01_swap_variable.js
+++ 01_swap_variable.js
@@ -1,9 +1,26 @@
 var temp;
-var num1=1000;
-var num2=2000;
-var num3=3000;
+var sweety="sweety";
+var cuetie="cutie";
+console.log("value before swapping======");
+console.log(sweety , cuetie);
+temp=sweety;
+sweety=cuetie;
+cuetie=temp;
+console.log("======value after swapping=====");
+console.log(sweety , cuetie);
+
+
+
+
+var temp;
+var num1=100;
+var num2=200;
+var num3=300;
 console.log("=====brfore swapping====")
-console.log(num1,num2,num3);
+console.log(num1,num2,num3)
+console.log("num1="+num1)
+console.log("num2="+num2)
+console.log("num3="+num3)
 console.log("====After swapping=====")
 temp=num1;
 num1=num2;
---
+++ 01_typeOf.js
@@ -0,0 +1,26 @@
+var bankName="SBI Bank"
+console.log(bankName)
+var bankType=  typeof bankName;
+console.log(bankType)
+console.log(typeof bankName)
+
+var mark='90%'
+var markType = typeof mark;
+console.log(markType)
+console.log(mark)
+
+var isWorking;
+console.log(isWorking)
+var isWorking = true;
+console.log( typeof isWorking)
+// var workingType = typeof isWorking
+// console.log(isWorking)
+
+var totalCount="Hundred  and seven"
+console.log(typeof totalCount)
+console.log(totalCount)
+var totalCount = 107;
+console.log(typeof totalCount)
+console.log(totalCount)
+
+
